c9ce5c41cc7bae4369a8019439b73ea5063a9b16
Diovana Cesconetto <cesconettodiovana2@gmail.com>
2020-11-15 21:42:54 -0400

    cidadeeditado

--- editarCidade.php
+++ editarCidade.php
@@ -6,11 +6,11 @@ $id_cidade = (isset($_GET['id_cidade'])) ? $_GET['id_cidade'] : '';
 
 if (!empty($id_cidade) && is_numeric($id_cidade)):
  
-	$sql = "SELECT b.id_cidade, b.nome, b.id_cidade FROM cidade AS b LEFT JOIN cidade AS c ON b.id_cidade = c.id_cidade WHERE id_cidade = '$id_cidade' ";
+	$sql = "SELECT id_cidade, nome, estado FROM cidade WHERE id_cidade = '$id_cidade' ";
 	$stm = $conn->prepare($sql);
 	$stm->execute();
 	$cidade = $stm->fetch(PDO::FETCH_OBJ);
- 
+
 endif;
  
 
@@ -47,23 +47,38 @@ endif;
         <div class="form-group">
           <div class="col-8">
 
-            <div class="form-group col-md-4">
-                <label for="inputCidade">Cidade</label>
-                <select class="form-control" id="id_cidade" name="id_cidade">
-          <?php 
-              $sql = "SELECT
-                        Id_cidade as id_cidade
-                          ,nome as nome
-                      from  cidade 
-                      order by Nome;";
-              $consulta = $conn->prepare( $sql );          
-              $consulta->execute();
-              $cidade = $consulta->fetchAll();
-
-              foreach ($cidade as $key => $value) {
-                echo "<option value='".$value['id_cidade']."'>". $value['nome']."</option>";
-              }
-          ?>
+          <div class="form-group col-md-4">
+              <label for="inputCidade">Estado</label>
+              <select id="estado" name="estado">
+                <option value="">Selecione</option>
+                <option value="AC">Acre</option>
+                <option value="AL">Alagoas</option>
+                <option value="AP">Amapá</option>
+                <option value="AM">Amazonas</option>
+                <option value="BA">Bahia</option>
+                <option value="CE">Ceará</option>
+                <option value="DF">Distrito Federal</option>
+                <option value="ES">Espirito Santo</option>
+                <option value="GO">Goiás</option>
+                <option value="MA">Maranhão</option>
+                <option value="MS">Mato Grosso do Sul</option>
+                <option value="MT">Mato Grosso</option>
+                <option value="MG">Minas Gerais</option>
+                <option value="PA">Pará</option>
+                <option value="PB">Paraíba</option>
+                <option value="PR">Paraná</option>
+                <option value="PE">Pernambuco</option>
+                <option value="PI">Piauí</option>
+                <option value="RJ">Rio de Janeiro</option>
+                <option value="RN">Rio Grande do Norte</option>
+                <option value="RS">Rio Grande do Sul</option>
+                <option value="RO">Rondônia</option>
+                <option value="RR">Roraima</option>
+                <option value="SC">Santa Catarina</option>
+                <option value="SP">São Paulo</option>
+                <option value="SE">Sergipe</option>
+                <option value="TO">Tocantins</option>
+</select>
 				    <input type="hidden" name="id_cidade" value="<?=$cidade->id_cidade?>">
 				    <button type="submit" class="btn btn-primary" id='botao'> 
 				      Gravar
